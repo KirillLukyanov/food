39e108c2c6f86905fdcf1250b7c5a9bb497cbbb5
Kirill <kirlukyanov@gmail.com>
2021-03-13 14:39:56 +0300

    gulp - OK

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,11 @@ gulp.task('server', function() {
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/scss/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
@@ -29,9 +30,10 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/scss/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
-})
+    gulp.watch("src/js/*.js").on('change', gulp.parallel('scripts'));
+});
 
 gulp.task('html', function () {
     return gulp.src("src/*.html")
