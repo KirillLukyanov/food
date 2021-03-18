b27e05c432c9185f109d915f20308a5d902a355e
Kirill <kirlukyanov@gmail.com>
2021-03-18 21:04:05 +0300

    modal window

--- src/js/main.js
+++ src/js/main.js
@@ -91,5 +91,37 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modalCloseBtn = document.querySelector('[data-close]'),
+          modal = document.querySelector('.modal');
+
+    function closeModal () {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+          modalTrigger.forEach(item => {
+        item.addEventListener('click', () => {
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
 });
 
